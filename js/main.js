bc2bd5d79a7666caab1b7ecbf2eb75b9ae891e58
raihan <5421449+ra89fi@users.noreply.github.com>
2022-06-11 01:00:35 +0600

    mailer link

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 function handleSubmit(e) {
     e.preventDefault();
+    const data = {};
+    const form = document.querySelector('form');
+    // const btn = document.querySelector('input[type="submit"]');
+    // console.log(form, btn);
+    // btn.disabled = true;
+    const formData = new FormData(form);
+    for (var pair of formData.entries()) {
+        // console.log(pair[0] + ': ' + pair[1]);
+        data[pair[0]] = pair[1];
+    }
+    // console.log('data', data);
     fetch(
-        'https://script.google.com/macros/s/AKfycbxbv-9WrGVrNYJ5QPDKO26b0Gxu8g9E6aoYFk--G7GM_TBf4oQuawR8O6i9eWdaqs5u/exec',
+        // 'https://script.google.com/macros/s/AKfycbwGEWitAqhpg1AcuykxSnxSxn8cVwt4H8ew82TkL7xBQ3L2M8OyupwLbuue3CqLP1C1/exec',
+        'https://script.google.com/macros/s/AKfycby3YrCCdfxHyTQpR3EZwElZg2gI4IR01ztWcIJYKA96fBFS6R_jjY2NxgAE352hJa0/exec',
+        // 'https://script.google.com/macros/s/AKfycbzRE7VQdFRLXrO7x6GQ5cOGLKSihbvWWQKKTAZo8hE/dev',
         {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'text/plain',
             },
             body: JSON.stringify(data),
         }
     )
         .then((data) => data.json())
         .then((result) => {
-            console.log(data);
+            // console.log(result);
         })
         .catch((err) => console.log(err.message))
-        .finally(() => {});
+        .finally(() => {
+            form.reset();
+            // btn.disabled = false;
+        });
 }
+
+window.onload = function () {
+    var form = document.querySelector('form');
+    form.onsubmit = handleSubmit.bind(form);
+};
